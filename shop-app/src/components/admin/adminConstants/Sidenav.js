be71ba997d0e96cc0bd41e90a3398c0cbad3e286
DatNguyen1091 <nvdat0204@gmail.com>
2023-08-03 22:53:01 +0700

    update

--- shop-app/src/components/admin/adminConstants/Sidenav.js
+++ shop-app/src/components/admin/adminConstants/Sidenav.js
@@ -3,28 +3,22 @@ import React from 'react';
 export const Sidenav = () => {
 
   return (
-    <nav className="col-md-3 col-lg-2 d-md-block bg-light sidebar">
-      <div className="position-sticky">
-        <ul className="nav flex-column">
-          <li className="nav-item">
-            <a className="nav-link active" href="#!">
-              Dashboard
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#!">
-              Users
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#!">
-              Products
-            </a>
-          </li>
-          {/* Add more items as needed */}
-        </ul>
+    <div className="container-flut">
+      <div className="row">
+        <div className="col-md-2 bg-dark text-white" style={{ minHeight: '100vh' }}>
+          <div className="list-group ">
+            <h5 className="list-group-item list-group-item-action list-group-item-dark">Bảng điều khiển</h5>
+            <a href="/admin/products" className="list-group-item list-group-item-action list-group-item-dark">Danh mục sản phẩm</a>
+            <a href="/admin/categories" className="list-group-item list-group-item-action list-group-item-dark">Quản lý sản phẩm</a>
+            <a href="/admin/accounts" className="list-group-item list-group-item-action list-group-item-dark">Quản lý tài khoảng</a>
+            <a href="/admin/customers" className="list-group-item list-group-item-action list-group-item-dark">Khách hàng</a>
+            <a href="/admin/carts" className="list-group-item list-group-item-action list-group-item-dark">Giỏ hàng</a>
+            <a href="/admin/orders" className="list-group-item list-group-item-action list-group-item-dark">Đơn hàng</a>
+            <a href="/admin/feedbacks" className="list-group-item list-group-item-action list-group-item-dark">Bình luận</a>
+          </div>
+        </div>
       </div>
-    </nav>
+    </div>
   );
 };
 
