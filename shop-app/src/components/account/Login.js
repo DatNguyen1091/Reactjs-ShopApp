c0a495e4658cc078aee830238759e31dfed219a7
DatNguyen1091 <nvdat0204@gmail.com>
2023-08-03 21:29:49 +0700

    Add Api Login

--- shop-app/src/components/account/Login.js
+++ shop-app/src/components/account/Login.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Footer2 from "../layout/constants/Footer2";
+import axios from 'axios';
+import { ApiUrl } from '../layout/constants/ApiUrl';
 
 const Login = () => {
+    const [setId] = useState(0);
+    const [username, setUsername] = useState('');
+    const [email] = useState('string');
+    const [password, setPassword] = useState('');
+    const [setIsAdmin] = useState(true);
+
+    const handleLogin = () => {
+        const loginData = {
+          id : setId,
+          username: username,
+          email: email,
+          password: password,
+          isAdmin: setIsAdmin,
+        };
+        axios.post(ApiUrl.Api + 'Users/Login', loginData)
+        .then(response => {
+          console.log(response.data);
+          alert("Đăng nhập thành công!");
+        })
+        .catch(error => {
+          console.error(error.response.data); 
+          alert("Đăng nhập thất bại!");
+        });
+    };
   return (
     <div>
         <header className="header_section" style={{backgroundColor: "aqua"}}>
@@ -43,23 +69,23 @@ const Login = () => {
 
                         <div className="input-group mb-3">
                         <span className="input-group-text">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-fill" viewBox="0 0 16 16">
                             <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
                             </svg>
                         </span>
-                        <input className="form-control" type="text" placeholder="Tên tài khoảng" />
+                        <input className="form-control" type="text" placeholder="Tên tài khoảng" value={username} onChange={(e) => setUsername(e.target.value)} />
                         </div>
 
                         <div className="input-group mb-3">
                             <span className="input-group-text">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-key-fill" viewBox="0 0 16 16">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-key-fill" viewBox="0 0 16 16">
                                 <path d="M3.5 11.5a3.5 3.5 0 1 1 3.163-5H14L15.5 8 14 9.5l-1-1-1 1-1-1-1 1-1-1-1 1H6.663a3.5 3.5 0 0 1-3.163 2zM2.5 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
                                 </svg>
                             </span>
-                            <input className="form-control" type="password" placeholder="Mật khẩu" />
+                            <input className="form-control" type="password" placeholder="Mật khẩu" value={password} onChange={(e) => setPassword(e.target.value)}/>
                         </div>
 
-                        <button className="btn btn-block btn-success" type="button">Đăng nhập</button>
+                        <button className="btn btn-block btn-success" type="button" onClick={handleLogin}>Đăng nhập</button>
                     </div>
                     </div>
                 </div>
