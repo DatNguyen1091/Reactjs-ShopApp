c8cac81e0f18fb45ca261c7708e38f438cb4b5d0
DatNguyen1091 <nvdat0204@gmail.com>
2023-08-05 12:00:27 +0700

    Handle Api GET/POST/PUT/DELETE Product

--- shop-app/src/components/admin/AdProduct.js
+++ shop-app/src/components/admin/AdProduct.js
@@ -154,15 +154,15 @@ class AdProduct extends Component {
             <Sidebar></Sidebar>
             {/* Heading */}
             <Paging></Paging>
-            <button type="button" className="btn btn-primary" style={{position:"absolute", left:"1200px", top:"120px"}}
+            <button type="button" className="btn btn-primary" style={{position:"absolute", left:"1370px", top:"120px"}}
             data-bs-toggle="modal"
             data-bs-target="#exampleModal"
             onClick={()=>this.addClick()}> 
             Thêm sản phẩm
             </button>
 
-            <h1 style={{position:"absolute", left:"570px", top:"80px"}}>Danh mục sản phẩm</h1>
-            <div style={{position:"absolute", left:"210px", top:"150px"}}>
+            <h1 style={{position:"absolute", left:"680px", top:"80px"}}>Danh mục sản phẩm</h1>
+            <div style={{position:"absolute", left:"240px", top:"150px"}}>
               <div className="container mt-3">  
                 <table className="table table-bordered table-hover" style={{border: "1px solid black"}}>
                   <thead>
@@ -193,7 +193,7 @@ class AdProduct extends Component {
                           <button type="button" className="bth bth-light mr-1"
                           data-bs-toggle="modal"
                           data-bs-target="#exampleModal"
-                          onClick={()=>this.editClick(product)}>
+                          onClick={()=>this.editProduct(product)}>
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
                             <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                               <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
